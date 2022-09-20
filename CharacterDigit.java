281eafa2f2bf45178abdba381e9d843b174a670a
Amit Kumar Agrawal <113965614+agamit0074@users.noreply.github.com>
2022-09-21 00:25:17 +0530

    Update CharacterDigit.java

--- CharacterDigit.java
+++ CharacterDigit.java
@@ -1,3 +1,4 @@
+//Program to find user entered string is character or digit using conditional operator....
 import java.util.Scanner;
 
 public class CharacterDigit {
@@ -6,7 +7,7 @@ public class CharacterDigit {
         System.out.println("Enter Character");
         char c = sc.next().charAt(0);
         System.out.println("----------------------------");
-        System.out.println("Result is : "+(((int)c>=48&&(int)c<=57)?"DIGIT":"CHARACTER"));
+        System.out.println("Result is : "+(((int)c>=48&&(int)c<=58)?"DIGIT":"CHARACTER"));
         System.out.println("----------------------------");
         sc.close();
     }
