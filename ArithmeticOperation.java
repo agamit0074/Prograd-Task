3719025dc82c197fa3c511592d18182cabbd37c0
Amit Kumar Agrawal <113965614+agamit0074@users.noreply.github.com>
2022-09-20 13:46:06 +0530

    Update ArithmeticOperation.java

--- ArithmeticOperation.java
+++ ArithmeticOperation.java
@@ -1,12 +1,15 @@
+// Program for to take two int input from user and Perform Arithematic operation.....
 import java.util.Scanner;
 
 public class ArithmeticOperation {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        // takeing input..
         System.out.println("Enter 1st No. :");
         int a = sc.nextInt();
         System.out.println("Enter 2nd No. :");
         int b = sc.nextInt();
+        //Arithmatic Opearations..
         System.out.println("----------------------------");
         System.out.println("Addition : "+(a+b));
         System.out.println("Subtract : "+(a-b));
